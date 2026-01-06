03392c8a44f92f2f879bdf1af687fcf2c514cbc2
SangHoonKim <fpdlsid9539@gmail.com>
2026-01-06 16:13:48 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -90,3 +90,32 @@ form.addEventListener("submit", (e) => {
 
 // ===== Footer Year =====
 $("#year").textContent = new Date().getFullYear();
+
+
+//이메일 전송 가능한 객체 가져오기.
+(function(){
+    //모든 html요소가 로드 되면 실행되는 함수.
+    emailjs.init("YicXk2Qxd5EPe6s8I");
+})();
+
+function sendMail(){
+            let from_name = document.querySelector("input[name = 'from_name']").value;
+            let name = "김상훈";
+            let to_name = "김상훈";
+            let email = "fpdlsid9539@gmail.com"
+            let message = document.querySelector("textarea[name = 'message']").value;
+
+            //parameter 방식으로 변환.
+            let params = {
+                from_name : from_name,
+                name : name,
+                to_name : to_name,
+                email : email,
+                massage : message
+            }
+
+            console.log(params);
+
+            emailjs.send("service_ohma2yq", "template_iisjubi", params).then(function(res){alert("이메일 발송이 완료되었습니다.")});
+            
+}
